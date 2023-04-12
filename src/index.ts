cae60390d70eb5dfa2d26d911330bc7a72ac10a0
Blayne Chard <bchard@linz.govt.nz>
2023-04-13 10:06:27 +1200

    build: switch to esm modules;

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { Nztm2000Quad } from './tms/nztm2000.quad';
-export { Nztm2000 } from './tms/nztm2000';
-export type { TileMatrixSetType, TileMatrixType } from './tile.matrix';
+export { Nztm2000Quad } from './tms/nztm2000.quad.js';
+export { Nztm2000 } from './tms/nztm2000.js';
+export type { TileMatrixSetType, TileMatrixType } from './tile.matrix.js';
